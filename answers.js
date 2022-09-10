1aa1365c4c96a14cdc269055024f55fe18a1fcec
Norman <whosnorm@gmail.com>
2022-09-10 19:11:15 -0400

    fizz buzz answered

--- answers.js
+++ answers.js
@@ -8,17 +8,23 @@
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
-for (i=0;i<=200;i++){
-    if(i%2==0){
-        console.log(i)
-    }
+// for (i=0;i<=200;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
     
-}
+// }
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
 
+// for(i=1; i<=100 ;i++)
+// {   if(i%5==0 && i%3==0){console.log("FizzBuzz")}
+//     else if(i%3==0){console.log("Fizz")}
+//     else if(i%5==0){console.log("Buzz")}
+//     else{ console.log(i)}
+// }
 
 ////////////////////////////////
 // Wild Wild Life
