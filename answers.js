7d420728d1fbdde882321169c7c2cd40ef635432
Norman <whosnorm@gmail.com>
2022-09-10 19:24:32 -0400

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -29,25 +29,31 @@
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
 
-plantee.splice(2, 1, 5001)
-console.log(plantee)
-wolfy.splice(3,1, "Gotham City")
-console.log(wolfy)
-dart.push("Hawkins")
-console.log(dart)
-wolfy.splice(0,1, "Gameboy")
-console.log(wolfy)
+// plantee.splice(2, 1, 5001)
+// console.log(plantee)
+// wolfy.splice(3,1, "Gotham City")
+// console.log(wolfy)
+// dart.push("Hawkins")
+// console.log(dart)
+// wolfy.splice(0,1, "Gameboy")
+// console.log(wolfy)
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
+const Turtles = [`Donatello`, `Leonardo`, `Raphael`, `Michaelangelo`]
+const newTurtles = []
 
+for(let turtle of Turtles){
+    newTurtles.push(turtle.toUpperCase())
+}
+console.log(newTurtles)
 
 ////////////////////////////////
 // Methods, Revisited
