601c4194bf54b46356a609886e46051443e1ad6c
Norman <whosnorm@gmail.com>
2022-09-12 10:16:08 -0400

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -19,31 +19,31 @@
 // Fizz Buzz
 ////////////////////////////////
 
-// for(i=1; i<=100 ;i++)
-// {   if(i%5==0 && i%3==0){console.log("FizzBuzz")}
-//     else if(i%3==0){console.log("Fizz")}
-//     else if(i%5==0){console.log("Buzz")}
-//     else{ console.log(i)}
-// }
+for(i=1; i<=100 ;i++)
+{   if(i%5==0 && i%3==0){console.log("FizzBuzz")}
+    else if(i%3==0){console.log("Fizz")}
+    else if(i%5==0){console.log("Buzz")}
+    else{ console.log(i)}
+}
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
 
-plantee.splice(2, 1, 5001)
-console.log(plantee)
-wolfy.splice(3,1, "Gotham City")
-console.log(wolfy)
-dart.push("Hawkins")
-console.log(dart)
-wolfy.splice(0,1, "Gameboy")
-console.log(wolfy)
+// plantee.splice(2, 1, 5001)
+// console.log(plantee)
+// wolfy.splice(3,1, "Gotham City")
+// console.log(wolfy)
+// dart.push("Hawkins")
+// console.log(dart)
+// wolfy.splice(0,1, "Gameboy")
+// console.log(wolfy)
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
